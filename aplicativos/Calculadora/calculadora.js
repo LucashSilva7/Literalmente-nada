e50485e28178c7f67a796945f88bed423788da44
Lucas <96627066+LucashSilva7@users.noreply.github.com>
2022-08-01 12:16:31 -0300

    TOMA CARAYP

--- aplicativos/Calculadora/calculadora.js
+++ aplicativos/Calculadora/calculadora.js
@@ -1,10 +1,10 @@
-
 let inputNumber = document.getElementById('input-number')
 let enviar = document.querySelector('.enviar');
 let addButton = document.querySelector('.add-button');
 let subButton = document.querySelector('.subtract-button');
 let multButton = document.querySelector('.multiplicar-button');
 let divButton= document.querySelector('.dividir-button');
+let currentResult = document.querySelector('#current-result');
 
 let defaultResult = 0;
 let resposta = 0;
@@ -12,26 +12,29 @@ let resposta = 0;
 //soma
 function add() {
     console.log(resposta = resposta + +inputNumber.value);
+    currentResult.textContent = resposta;
     return resposta;
 }
-
 //subtrai
 function subtrair() {
     console.log(resposta = resposta - +inputNumber.value);
+    currentResult.textContent = resposta;
     return resposta;
 }
-
 //multiplicar
 function multiplicar() {
     console.log(resposta = resposta * +inputNumber.value);
+    currentResult.textContent = resposta;
     return resposta;
 }
-
 //dividir
 function dividir() {
     console.log(resposta = resposta / +inputNumber.value);
+    currentResult.textContent = resposta;
     return resposta;
 }
+
+
 
 function result () {
     alert(resposta);
